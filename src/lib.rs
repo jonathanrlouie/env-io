5da03ccab7c3dcdc50f9d018a7a6a3a1d50aac65
Jonathan Louie <jonathanrlouie@gmail.com>
2019-03-30 13:12:01 -0700

    Add map and flatten functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,19 @@ pub trait EnvIO {
 
     fn run(self) -> Self::Out;
 
+    fn map<Out, F>(self, f: F) -> Map<Self, F>
+        where Self: Sized, F: Fn(Self::Out) -> Out {
+        Map { envio: self, f }
+    }
+
     fn flat_map<OutEnv, F>(self, f: F) -> FlatMap<Self, F>
-    where Self: Sized, OutEnv: EnvIO, F: Fn(Self::Out) -> OutEnv {
+        where Self: Sized, OutEnv: EnvIO, F: Fn(Self::Out) -> OutEnv {
         FlatMap { envio: self, f }
+    }
+
+    fn flatten(self) -> FlatMap<Self, fn(Self::Out) -> Self::Out>
+        where Self: Sized {
+        FlatMap { envio: self, f: move |x| x }
     }
 }
 
@@ -18,6 +28,19 @@ impl<Out0, F: Fn() -> Out0> EnvIO for Effect<Out0, F> {
 
     fn run(self) -> Self::Out {
         (self.effect)()
+    }
+}
+
+pub struct Map<Env, F> {
+    envio: Env,
+    f: F
+}
+
+impl<Env: EnvIO, Out, F> EnvIO for Map<Env, F> where F: Fn(Env::Out) -> Out {
+    type Out = Out;
+
+    fn run(self) -> Out {
+        (self.f)(self.envio.run())
     }
 }
 
@@ -57,3 +80,36 @@ macro_rules! eff {
         (move || $e).effect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use mdo::mdo;
+    use super::*;
+
+    fn bind<Env, OutEnv, F>(envio: Env, f: F) -> FlatMap<Env, F>
+        where Env: EnvIO, OutEnv: EnvIO, F: Fn(Env::Out) -> OutEnv,
+    {
+        envio.flat_map(f)
+    }
+
+    #[test]
+    fn test_flatten() {
+        let e = mdo! {
+            x =<< eff!(eff!("hi")).flatten();
+            ret eff!(x)
+        };
+        assert_eq!(e.run(), "hi");
+    }
+
+    #[test]
+    fn test_map() {
+        let e = mdo! {
+            x =<< eff!(2u32)
+                .map(|i: u32| 2 * i)
+                .map(|i: u32| i as i32);
+            ret eff!(x)
+        };
+        assert_eq!(e.run(), 4);
+    }
+}
+
