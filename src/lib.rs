bbb59cc78448e45ba3c18b4b7329566d988c5f0d
Jonathan Louie <jonathanrlouie@gmail.com>
2019-04-18 21:17:55 -0700

    Changed IO effect for Reader effect (messy workspace)

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,42 @@
+use std::marker::PhantomData;
+
 pub trait EnvIO {
+    type In;
     type Out;
 
-    fn run(self) -> Self::Out;
-
-    fn map<Out, F>(self, f: F) -> Map<Self, F>
-        where Self: Sized, F: Fn(Self::Out) -> Out {
-        Map { envio: self, f }
-    }
+    fn run(self, input: &Self::In) -> Self::Out;
 
     fn flat_map<OutEnv, F>(self, f: F) -> FlatMap<Self, F>
         where Self: Sized, OutEnv: EnvIO, F: Fn(Self::Out) -> OutEnv {
         FlatMap { envio: self, f }
     }
 
+    fn map<Out, F>(self, f: F) -> Map<Self, F>
+        where Self: Sized, F: Fn(Self::Out) -> Out {
+        Map { envio: self, f }
+    }
+
     fn flatten(self) -> FlatMap<Self, fn(Self::Out) -> Self::Out>
         where Self: Sized {
         FlatMap { envio: self, f: move |x| x }
     }
+
 }
 
-pub struct Effect<Out, F: Fn() -> Out> {
-    effect: F
+pub struct Effect<In, Out, F: Fn(&In) -> Out> {
+    effect: F,
+    _pd: PhantomData<In>
 }
 
-impl<Out0, F: Fn() -> Out0> EnvIO for Effect<Out0, F> {
+impl<In, Out0, F: Fn(&In) -> Out0> EnvIO for Effect<In, Out0, F> {
+    type In = In;
     type Out = Out0;
 
-    fn run(self) -> Self::Out {
-        (self.effect)()
+    fn run(self, input: &Self::In) -> Self::Out {
+        (self.effect)(input)
     }
 }
+
 
 pub struct Map<Env, F> {
     envio: Env,
@@ -37,61 +44,121 @@ pub struct Map<Env, F> {
 }
 
 impl<Env: EnvIO, Out, F> EnvIO for Map<Env, F> where F: Fn(Env::Out) -> Out {
+    type In = Env::In;
     type Out = Out;
 
-    fn run(self) -> Out {
-        (self.f)(self.envio.run())
+    fn run(self, input: &Self::In) -> Out {
+        (self.f)(self.envio.run(input))
     }
 }
+
+
 
 pub struct FlatMap<Env, F> {
     envio: Env,
     f: F
 }
 
-impl<Env: EnvIO, OutEnv: EnvIO, F> EnvIO for FlatMap<Env, F> where F: Fn(Env::Out) -> OutEnv {
+impl<Env: EnvIO, OutEnv: EnvIO<In=Env::In>, F> EnvIO for FlatMap<Env, F> where F: Fn(Env::Out) -> OutEnv {
+    type In = Env::In;
     type Out = OutEnv::Out;
 
-    fn run(self) -> OutEnv::Out {
-        (self.f)(self.envio.run()).run()
+    fn run(self, input: &Self::In) -> OutEnv::Out {
+        (self.f)(self.envio.run(input)).run(input)
     }
 }
 
-pub trait IntoEnvIO {
+pub trait IntoEnvIO<In> {
     type Out;
 
-    type IntoEnv: EnvIO<Out=Self::Out>;
+    type IntoEnv: EnvIO<In=In, Out=Self::Out>;
 
     fn effect(self) -> Self::IntoEnv;
 }
 
-impl<T, F: Fn() -> T> IntoEnvIO for F {
+impl<In, T, F: Fn(&In) -> T> IntoEnvIO<In> for F {
     type Out = T;
-    type IntoEnv = Effect<T, F>;
+    type IntoEnv = Effect<In, T, F>;
 
     fn effect(self) -> Self::IntoEnv {
-        Effect { effect: self }
+        Effect { effect: self, _pd: PhantomData }
     }
 }
 
+pub fn eff<A, B, F: Fn(&A) -> B>(f: F) -> Effect<A, B, F> {
+    f.effect()
+}
+
+/*
 #[macro_export]
 macro_rules! eff {
-    ($e:expr) => {
-        (move || $e).effect()
+    ($t:ty, $e:expr) => {
+        (move |_: &$t| $e).effect()
     }
-}
+}*/
 
 #[cfg(test)]
 mod tests {
     use mdo::mdo;
     use super::*;
 
+    /*
     fn bind<Env, OutEnv, F>(envio: Env, f: F) -> FlatMap<Env, F>
         where Env: EnvIO, OutEnv: EnvIO, F: Fn(Env::Out) -> OutEnv,
     {
         envio.flat_map(f)
+    }*/
+
+    trait Console<T> {
+        fn println(&self, line: &str) -> T;
     }
 
+    struct ProductionConsole;
+
+    struct ConsoleModule<T> {
+        console: T
+    }
+
+    impl Console<()> for ProductionConsole {
+        fn println(&self, line: &str) {
+            println!("{}", line)
+        }
+    }
+
+    struct TestConsole;
+
+    impl Console<String> for TestConsole {
+        fn println(&self, line: &str) -> String {
+            line.to_string()
+        }
+    }
+
+    fn println<A, T: Console<A>>(line: String) -> impl EnvIO<In=ConsoleModule<T>, Out=A> {
+        eff(move |console_mod: &ConsoleModule<T>| console_mod.console.println(&line))
+    }
+
+    #[test]
+    fn test() {
+        //println!("hi");
+        //let e = eff(|a: &i32| println!("hi {}", a.to_string()));
+        //let e = eff!(i32, "hi");
+
+        /*
+        let e = mdo! {
+            _ =<< println("Hi world");
+            ret println("yo")
+        };*/
+
+        let e = println("hi".to_string())
+            .flat_map(|_| println("yo".to_string()));
+
+        let console_mod = ConsoleModule {
+            console: TestConsole
+        };
+        assert_eq!(e.run(&console_mod), "yo");
+    }
+
+    /*
     #[test]
     fn test_flatten() {
         let e = mdo! {
@@ -110,6 +177,6 @@ mod tests {
             ret eff!(x)
         };
         assert_eq!(e.run(), 4);
-    }
+    }*/
 }
 
