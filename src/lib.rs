ac78217aff6bd2ce2260bdb5f6527a9156b7ab09
Jonathan Louie <jonathanrlouie@gmail.com>
2019-07-27 17:43:44 -0700

    Resolve merge conflicts with master

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,88 @@
 use std::marker::PhantomData;
+use std::mem;
 
-pub trait EnvIO {
-    type In;
-    type Out;
-
-    fn run(self, input: &Self::In) -> Self::Out;
-
-    fn flat_map<OutEnv, F>(self, f: F) -> FlatMap<Self, F>
-        where Self: Sized, OutEnv: EnvIO, F: Fn(Self::Out) -> OutEnv {
-        FlatMap { envio: self, f }
-    }
-
-    fn map<Out, F>(self, f: F) -> Map<Self, F>
-        where Self: Sized, F: Fn(Self::Out) -> Out {
-        Map { envio: self, f }
-    }
-
-    fn flatten(self) -> FlatMap<Self, fn(Self::Out) -> Self::Out>
-        where Self: Sized {
-        FlatMap { envio: self, f: move |x| x }
-    }
-
+struct Kleisli<R, A, E> {
+    k: Box<dyn Fn(Box<Opaque>) -> EnvIO<Opaque, Opaque, Opaque>>,
+    _pd: PhantomData<(R, A, E)>
 }
 
-pub struct Effect<In, Out, F: Fn(&In) -> Out> {
-    effect: F,
-    _pd: PhantomData<In>
+enum Opaque {}
+
+enum EnvIO<R, A, E> {
+    Effect(Box<dyn Fn() -> Box<A>>),
+    Succeed(Box<A>),
+    Fail(Box<E>),
+    Provide(Box<R>),
+    FlatMap(Box<EnvIO<R, Opaque, E>>, Kleisli<R, A, E>)
 }
 
-impl<In, Out0, F: Fn(&In) -> Out0> EnvIO for Effect<In, Out0, F> {
-    type In = In;
-    type Out = Out0;
+impl<R, A, E> EnvIO<R, A, E> {
+    unsafe fn flat_map_unsafe<B>(self, k: Box<dyn Fn(Box<A>) -> EnvIO<R, B, E>>) -> EnvIO<R, B, E> {
+        EnvIO::FlatMap(Box::new(mem::transmute(self)), Kleisli { k: mem::transmute(k), _pd: PhantomData })
+    }
 
-    fn run(self, input: &Self::In) -> Self::Out {
-        (self.effect)(input)
+    fn flat_map<B, F: 'static>(self, k: F) -> EnvIO<R, B, E> where F: Fn(A) -> EnvIO<R, B, E> {
+        unsafe { self.flat_map_unsafe(Box::new(move |a: Box<A>| k(*a))) }
     }
 }
 
-
-pub struct Map<Env, F> {
-    envio: Env,
-    f: F
-}
-
-impl<Env: EnvIO, Out, F> EnvIO for Map<Env, F> where F: Fn(Env::Out) -> Out {
-    type In = Env::In;
-    type Out = Out;
-
-    fn run(self, input: &Self::In) -> Out {
-        (self.f)(self.envio.run(input))
-    }
-}
-
-
-
-pub struct FlatMap<Env, F> {
-    envio: Env,
-    f: F
-}
-
-impl<Env: EnvIO, OutEnv: EnvIO<In=Env::In>, F> EnvIO for FlatMap<Env, F> where F: Fn(Env::Out) -> OutEnv {
-    type In = Env::In;
-    type Out = OutEnv::Out;
-
-    fn run(self, input: &Self::In) -> OutEnv::Out {
-        (self.f)(self.envio.run(input)).run(input)
-    }
-}
-
-pub trait IntoEnvIO<In> {
-    type Out;
-
-    type IntoEnv: EnvIO<In=In, Out=Self::Out>;
-
-    fn effect(self) -> Self::IntoEnv;
-}
-
-impl<In, T, F: Fn(&In) -> T> IntoEnvIO<In> for F {
-    type Out = T;
-    type IntoEnv = Effect<In, T, F>;
-
-    fn effect(self) -> Self::IntoEnv {
-        Effect { effect: self, _pd: PhantomData }
-    }
-}
-
-pub fn eff<A, B, F: Fn(&A) -> B>(f: F) -> Effect<A, B, F> {
-    f.effect()
-}
-
-#[macro_export]
-macro_rules! eff {
+macro_rules! effect {
     ($e:expr) => {
-        (move |_: &()| $e).effect()
+        {
+			$crate::effect(move || Box::new($e))
+        }
+    }
+}
+
+fn effect<R, A, E, F: 'static>(eff: F) -> EnvIO<R, A, E> where F: Fn() -> Box<A> {
+    EnvIO::Effect(Box::new(eff))
+}
+
+fn succeed<R, A, E>(a: A) -> EnvIO<R, A, E> {
+    EnvIO::Succeed(Box::new(a))
+}
+
+fn run<R, A, E>(envio: EnvIO<R, A, E>) -> A {
+    unsafe {
+        let envio_opaque: EnvIO<Opaque, Opaque, Opaque> = mem::transmute(envio);
+        interpret(envio_opaque)
+    }
+}
+
+unsafe fn interpret<A>(mut envio: EnvIO<Opaque, Opaque, Opaque>) -> A {
+    let mut stack: Vec<Kleisli<Opaque, Opaque, Opaque>> = vec![];
+    loop {
+        match envio {
+            EnvIO::FlatMap(inner, kleisli) => {
+                match *inner {
+                    EnvIO::Effect(eff) => {
+                        envio = (kleisli.k)(eff())
+                    }
+                    EnvIO::Succeed(a) => {
+                        envio = (kleisli.k)(a);
+                    }
+                    _ => {
+                        stack.push(kleisli);
+                        envio = *inner;
+                    }
+                }
+            },
+            EnvIO::Effect(eff) => {
+                if let Some(kleisli) = stack.pop() {
+                    envio = (kleisli.k)(eff());
+                } else {
+                    return *mem::transmute::<Box<Opaque>, Box<A>>(eff());
+                }
+            }
+            EnvIO::Succeed(a) => {
+                if let Some(kleisli) = stack.pop() {
+                    envio = (kleisli.k)(a);
+                } else {
+                    return *mem::transmute::<Box<Opaque>, Box<A>>(a);
+                }
+            },
+            _ => panic!("unknown instruction")
+        }
     }
 }
 
@@ -101,12 +91,11 @@ mod tests {
     use mdo::mdo;
     use super::*;
 
-    fn bind<Env, OutEnv, F>(envio: Env, f: F) -> FlatMap<Env, F>
-        where Env: EnvIO, OutEnv: EnvIO, F: Fn(Env::Out) -> OutEnv,
-    {
-        envio.flat_map(f)
+    #[test]
+    fn test() {
+        println!("hi")
     }
-
+    /*
     trait Console<T> {
         fn println(&self, line: &str) -> T;
     }
@@ -166,6 +155,6 @@ mod tests {
             ret eff!(x)
         };
         assert_eq!(e.run(&()), 4);
-    }
+    }*/
 }
 
